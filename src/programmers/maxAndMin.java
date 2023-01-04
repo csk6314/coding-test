31c9d7a1e121b091b2aded7025b336b4867205f0
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-01-05 00:17:10 +0900

    프로그래머스 - 최대값 최소값 ( Level 2 )

---
+++ src/programmers/maxAndMin.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class maxAndMin {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(br.readLine()));
+    }
+    static String solution(String s) {
+        String answer = "";
+        String[] s_values = s.split(" ");
+        int size = s_values.length;
+        int[] values = new int[size];
+        for(int i =0;i<size;i++) {
+            values[i] = Integer.parseInt(s_values[i]);
+        }
+        Arrays.sort(values);
+        answer = values[0] + " " + values[size-1];
+        return answer;
+    }
+}
