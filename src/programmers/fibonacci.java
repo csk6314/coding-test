1900ba7ee8a27d10e7ca67414cb1049e99fd315f
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-01-05 20:07:40 +0900

    프로그래머스 - 피보나치 ( Level 2 )

---
+++ src/programmers/fibonacci.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class fibonacci {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(solution(N));
+    }
+    static Map<Integer, Integer> map = new HashMap<>();
+    public static int solution(int n) {
+        int answer = 0;
+        answer = fibo(n);
+        return answer;
+    }
+    public static int fibo(int n) {
+        if (n==0) {
+            return 0;
+        }
+        if (n==1) {
+            return 1;
+        }
+        if(map.containsKey(n)) {
+            return map.get(n);
+        }
+        int ans = ((fibo(n-1)) + (fibo(n-2)))%1234567;
+        map.put(n,ans);
+        return ans;
+    }
+}
