965940db52ee5c6c3ced9d930022c46b33398053
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-01-05 22:22:53 +0900

    프로그래머스 - 다음 큰 수 ( Level 2 )

---
+++ src/programmers/nextBigNumber.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class nextBigNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(solution(N));
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        int size = getSize(n);
+        while (getSize(++n) != size) {}
+        answer = n;
+        return answer;
+    }
+    public static int getSize(int n) {
+        if (n==0) return 0;
+        return n%2 + getSize(n/2);
+    }
+}
