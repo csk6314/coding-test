e29e45ce442ae245dd1539c451e5fecb84597ee4
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-01-05 17:54:46 +0900

    프로그래머스 - 최솟값 만들기 ( Level 2 )

---
+++ src/programmers/makeMin.java
@@ -0,0 +1,43 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class makeMin {
+    public static void main(String[] args) {
+        int[] A = {1,4,2};
+        int[] B = {5,4,4};
+        System.out.println(solution(A,B));
+    }
+    public static int solution(int []A, int []B)
+    {
+        int answer = 0;
+        int size = A.length;
+        int a_start,a_end,b_start,b_end;
+
+        a_start = 0;
+        b_start = 0;
+        a_end = size-1;
+        b_end = size-1;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i =0;i<size;i++) {
+            int temp1 = A[a_start] * B[b_end];
+            int temp2 = A[a_end] * B[b_start];
+            if (temp1 < temp2) {
+                answer += temp1;
+                a_start++;
+                b_end--;
+                continue;
+            }
+            if( temp1 >= temp2){
+                answer += temp2;
+                a_end--;
+                b_start++;
+                continue;
+            }
+        }
+        return answer;
+    }
+}
