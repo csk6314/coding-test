fdf9079adeb2649d239ded09e309c2eb0cef7725
채승규 <37896060+csk6314@users.noreply.github.com>
2025-01-31 14:45:47 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 460 ms, Memory: 84196 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -3,10 +3,11 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 	static int N,M;
 	static boolean[][] map,visited;
 	static int[][] dp;
+	static Queue<int[]> edges = new LinkedList<>();
 	static int[] dr = {1,0,-1,0};
 	static int[] dc = {0,1,0,-1};
 	
@@ -36,43 +37,25 @@ public class Main{
 			}
 		}
 		
-		int group = 0;
-		ArrayList<Integer> groupSize = new ArrayList<>();
-		
+		// grouping with size
 		for(int i =0;i<N;i++) {
 			for(int j =0;j<M;j++) {
 				if(!visited[i][j] && map[i][j]) {
-					int size = dfs(i,j,group) % 10;
+					int size = dfs(i,j);
 					
-					groupSize.add(size);
-					group++;
-					
-				}
-			}
-		}
-		
-		for(int i = 0;i<N;i++) {
-			for(int j = 0;j<M;j++) {
-				if(!map[i][j]) {
-					boolean[] groupCheck = new boolean[group+1];
-					for(int k = 0;k<4;k++) {
-						int nr = i + dr[k];
-						int nc = j + dc[k];
+					while(!edges.isEmpty()) {
+						int[] p = edges.poll();
 						
-						if(nr<0 || nr>= N || nc < 0 || nc>=M) continue;
-				
-						if(map[nr][nc] && !groupCheck[dp[nr][nc]]) {
-							dp[i][j] += groupSize.get(dp[nr][nc]);
-							groupCheck[dp[nr][nc]] = true;
-						}
-						
+						dp[p[0]][p[1]] += size;
+						dp[p[0]][p[1]] %= 10;
+						visited[p[0]][p[1]] =false;
 					}
 					
-					dp[i][j] %= 10;
 				}
 			}
 		}
 		
+		//출력
 		StringBuilder sb = new StringBuilder();
 		
 		for(int i =0;i<N;i++) {
@@ -90,11 +73,10 @@ public class Main{
 		System.out.println(sb.toString());
 	}
 	
-	public static int dfs(int r, int c, int group) {
+	public static int dfs(int r, int c) {
 		if(visited[r][c]) return 0;
 		
 		visited[r][c] = true;
-		dp[r][c] = group;
 		
 		int size = 1;
 		
@@ -103,10 +85,15 @@ public class Main{
 			int nc = c + dc[i];
 			
 			if(nr<0 || nr>= N || nc < 0 || nc>=M) continue;
+			if(visited[nr][nc]) continue;
 			
-			if(!visited[nr][nc] && map[nr][nc]) {
-				size += dfs(nr,nc,group);
+			if(map[nr][nc]) {
+				size += dfs(nr,nc);
+				continue;
 			}
+			
+			edges.add(new int[] {nr,nc});
+			visited[nr][nc] = true;
 		}
 		
 		return size;
