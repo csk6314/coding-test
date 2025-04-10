28c6f60a86726d1ab0967d8939160761c6998dc2
채승규 <37896060+csk6314@users.noreply.github.com>
2025-04-10 23:50:56 +0900

    [Silver I] Title: 볼 모으기, Time: 196 ms, Memory: 17684 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -3,7 +3,7 @@
 import java.io.*;
 
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -13,26 +13,69 @@ public class Main{
 		
 		int bCount = 0;
 		int rCount = 0;
+		int f_bCount = 0;
+		int f_rCount = 0;
 		
-		int rCnt = 0;
-		int bCnt = 0;
+		int ans = Integer.MAX_VALUE;
+		boolean FLAG = false;
 		
-		for(int i = 0;i<balls.length();i++) {
+		for(int i = balls.length()-1;i>=0;i--) {
 			char ch = balls.charAt(i);
 			
-			if(ch=='R') {
-				rCnt ++;
-				bCount += bCnt;
-				bCnt = 0;
+			if(ch=='R' && FLAG) {
+				rCount ++;
 			}
 			
 			if(ch=='B') {
-				bCnt++;
-				rCount += rCnt;
-				rCnt = 0;
+				FLAG = true;
 			}
 		}
 		
-		System.out.println(Math.min(rCount, bCount));
+		FLAG = false;
+		for(int i = balls.length()-1;i>=0;i--) {
+			char ch = balls.charAt(i);
+			
+			if(ch=='B' && FLAG) {
+				bCount ++;
+			}
+			
+			if(ch=='R') {
+				FLAG = true;
+			}
+		}
+		
+		ans = Math.min(rCount, bCount);
+		
+		FLAG = false;
+		
+		for(int i = 0;i < balls.length();i++) {
+			char ch = balls.charAt(i);
+			
+			if(ch=='R' && FLAG) {
+				f_rCount ++;
+			}
+			
+			if(ch=='B') {
+				FLAG = true;
+			}
+		}
+		
+		FLAG = false;
+		
+		for(int i = 0;i < balls.length();i++) {
+			char ch = balls.charAt(i);
+			
+			if(ch=='B' && FLAG) {
+				f_bCount ++;
+			}
+			
+			if(ch=='R') {
+				FLAG = true;
+			}
+		}
+		
+		ans = Math.min(ans, Math.min(f_rCount, f_bCount));
+		
+		System.out.println(ans);
 	}
 }
